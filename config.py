0e511c7dc1abdea8c1c2408040d9582edad7420f
d3fuse99 <d3fuse99@gmail.com>
2026-08-09 13:23:00 +0300

    Add application configuration settings

---
+++ config.py
@@ -0,0 +1,11 @@
+SERVER_HOST = "0.0.0.0"
+SERVER_PORT = 8000
+CACHE_DB_FILE = "cache.db"
+CACHE_TTL_DAYS = 7
+MAX_WORKERS = 10
+IP_API_URL = "http://ip-api.com/json/"
+DNSBL_LIST = [
+    "b.barracudacentral.org",
+    "bl.spamcop.net",
+    "dnsbl.sorbs.net"
+]
