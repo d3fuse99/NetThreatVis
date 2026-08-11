66d883dead59ec612c84358a88b1535018e60454
d3fuse <d3fuse99@gmail.com>
2026-08-11 12:41:28 +0300

    Delete visualizer.py

--- visualizer.py
+++
@@ -1,9 +0,0 @@
-import sys
-from main import main
-
-if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        print("\n[!] Server stopped by user.")
-        sys.exit(0)
