060e027be7dcb0fe3bfeb02e86bb81b5e0dedadb
d3fuse99 <d3fuse99@gmail.com>
2026-08-11 12:38:40 +0300

    config: add batch geoip endpoint and update dnsbl list

--- config.py
+++ config.py
@@ -4,10 +4,11 @@ CACHE_DB_FILE = "cache.db"
 CACHE_TTL_DAYS = 7
 MAX_WORKERS = 10
 IP_API_URL = "http://ip-api.com/json/"
+IP_API_BATCH_URL = "http://ip-api.com/batch"
 DNSBL_LIST = [
-    "b.barracudacentral.org",
     "bl.spamcop.net",
-    "dnsbl.sorbs.net"
+    "dnsbl.sorbs.net",
+    "zen.spamhaus.org"
 ]
 LIVE_REFRESH_SECONDS = 5
 HIGH_THREAT_THRESHOLD = 60
