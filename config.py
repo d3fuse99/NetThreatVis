ad7205cc393d1180e30d4e7993c383a5dec69d36
d3fuse <d3fuse99@gmail.com>
2026-08-11 20:08:13 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+SERVER_HOST = "0.0.0.0"
+SERVER_PORT = 8000
+CACHE_DB_FILE = "cache.db"
+CACHE_TTL_DAYS = 7
+MAX_WORKERS = 10
+IP_API_URL = "http://ip-api.com/json/"
+IP_API_BATCH_URL = "http://ip-api.com/batch"
+DNSBL_LIST = [
+    "bl.spamcop.net",
+    "dnsbl.sorbs.net",
+    "zen.spamhaus.org"
+]
+LIVE_REFRESH_SECONDS = 5
+HIGH_THREAT_THRESHOLD = 60
+MED_THREAT_THRESHOLD = 30
